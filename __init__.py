392095828a7e6e79d8b584beeb6a61601ec929ce
Shwerzb <153048058+Shwerzb@users.noreply.github.com>
2024-11-04 08:59:05 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
+import asyncio
+import logging
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "hyundai_bluelink"
+LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    # Initialize Bluelinky client here
+    hass.data[DOMAIN] = BluelinkyClient(entry.data)
+    return True
